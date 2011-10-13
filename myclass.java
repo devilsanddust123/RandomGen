bee28ddac8c67e1b95e5afe9d13d2b25edaffd88
Shiva Garg <chivagarg@gmil.com>
2011-10-13 11:33:58 +0000

    first commit

---
+++ myclass.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class myclass{
+   public static void main(String[] args){
+	double num=Math.random();
+	if(num<=0.33)
+		System.out.print("1");
+	else if(num <=0.66)
+				System.out.print("2");
+	else
+		System.out.print("3");
+	}
+}
